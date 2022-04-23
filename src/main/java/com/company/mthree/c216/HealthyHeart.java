bf2a4a2f15e91fe342a4afcf3a83aeb1ea2c542f
sobia bushra <68123576+sobia-shahid@users.noreply.github.com>
2022-04-23 14:09:28 +0100

    updated code

--- src/main/java/com/company/mthree/c216/HealthyHeart.java
+++ src/main/java/com/company/mthree/c216/HealthyHeart.java
@@ -1,4 +1,4 @@
-package com.company.mthree.c216;
+ package com.company.mthree.c216;
 
 import java.util.Scanner;
 
@@ -15,8 +15,8 @@ public class HealthyHeart {
         /* function to find calculate the heart rate
            and range of healthy heart */
         int rate = 220 - a;
-       int max_limit = (int) (0.85 * rate);
-        System.out.println("Your maximum heart rate should be "+ max_limit +"  beats per minute");
-        System.out.println("Your target HR Zone is "+ Math.abs(50-max_limit)+" - "+ max_limit+" beats per minute");
+        int max_limit = (int) (0.85 * rate);
+        System.out.println("Your maximum heart rate should be "+ rate +"  beats per minute");
+        System.out.println("Your target HR Zone is "+(int)(0.50 * rate) +" - "+ max_limit+" beats per minute");
     }
 }
